faffcfe684c1e6dc6db5700311ebace8363a942d
JENNIFER754-DEL <jennifernyaguthii23@gmail.com>
2025-04-25 16:27:55 +0300

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
+
+  function addQuestion(newQuestion) {
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions((questions) => [...questions, data]);
+        setPage("List");
+      })
+      .catch((error) => console.error("Error adding question:", error));
+  }
+
+  function deleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuestions((questions) => questions.filter((q) => q.id !== id));
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  }
+
+  function updateQuestion(id, updatedFields) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedFields),
+    })
+      .then((res) => res.json())
+      .then((updatedQuestion) => {
+        setQuestions((questions) =>
+          questions.map((q) => (q.id === id ? updatedQuestion : q))
+        );
+      })
+      .catch((error) => console.error("Error updating question:", error));
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={addQuestion} setPage={setPage} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDelete={deleteQuestion}
+          onUpdate={updateQuestion}
+        />
+      )}
     </main>
   );
 }
