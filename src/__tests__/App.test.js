8d9a7838e62371ff2614919f2699881f1affc0ed
Mcadams <mcadamsproject@gmail.com>
2025-07-29 16:44:38 +0300

    unit4

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,6 +6,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { server } from "../mocks/server";
 import { resetQuestions } from "../mocks/handlers";
 
